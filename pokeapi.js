7efc6642d508a1c790473cf73295a712634e2bfd
Egor <zangedzu333gig@gmail.com>
2018-10-03 19:13:25 +0300

    Task 01_2

--- pokeapi.js
+++ pokeapi.js
@@ -7,3 +7,20 @@ axios.get('https://pokeapi.co/api/v2/pokemon/42')
     }).catch((err) => {
         console.log(`Error load pokemon with id 42: ${err}`);
     });
+
+
+let request = [];
+const count = 10;
+for (let i=0; i<3; i++)
+    request.push(axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=${count}&offset=${count*i}`));
+
+Promise.all(request)
+    .then((res) => {
+        res.forEach((val, resInd)=>{
+            val.data.results.forEach((pokemon, ind) => {
+                console.log(`${resInd}-${ind}: ${pokemon.name}`);
+            })
+        })
+    }).catch((err) => {
+        console.log(`Error load pokemons 3 x 10: ${err}`);
+    });
